e7304637c9d618fc3b73f9f249337c6112baf186
dmitrii <dmitrii@nsynk.de>
2020-06-09 20:11:00 +0200

    fix build problems

--- Source/AugmentedUnreality/AURDriver.h
+++ Source/AugmentedUnreality/AURDriver.h
@@ -17,7 +17,6 @@ limitations under the License.
 #pragma once
 
 #include "video_sources/AURVideoSource.h"
-#include "HAL/PlatformFilemanager.h"
 #include "AURDriver.generated.h"
 
 class AAURFiducialPattern;
